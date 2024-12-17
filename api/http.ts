84e9402fbcc8ccf142159f675ba5b09475f66ad0
Dev Ttangkong <ttankkeo112@gmail.com>
2024-12-17 13:37:07 +0900

    Added APILength enum in api/http.ts

--- api/http.ts
+++ api/http.ts
@@ -3,3 +3,9 @@ export enum APIException {
     INVALID_REQUEST_METHOD = "invalid_request_method",
     MISSING_REQUEST_FORMAT = "missing_request_format"
 }
+
+export enum APILength {
+    alias = 64,
+    email = 255,
+    displayName = 32
+}
