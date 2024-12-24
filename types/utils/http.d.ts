890a24d449e51528a9d6a1ff7e8317170e36276d
Dev Ttangkong <ttankkeo112@gmail.com>
2024-12-25 01:28:32 +0900

    Update files about /types as ts-build

--- types/utils/http.d.ts
+++ types/utils/http.d.ts
@@ -1,5 +1,5 @@
 import { ServerResponse } from "http";
 export declare class HTTPUtil {
     static contentTypeOf(path: string): string;
-    static parseRequest<T>(buffer: Buffer, response: ServerResponse): T | null;
+    static parseRequest<T>(given: Buffer | string, response: ServerResponse): T | null;
 }
